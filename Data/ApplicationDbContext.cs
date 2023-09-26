292e19cc677ce97d1fe58ba461f8e499c75022f8
wesleyruchel <wesleyruchel@gmail.com>
2023-09-26 17:37:39 -0300

    Add db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using crud_aspnet.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace crud_aspnet.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; } = default!;
+    public DbSet<Group> Groups { get; set; } = default!;
+    public DbSet<SubGroup> SubGroups { get; set; } = default!;
+}
---
+++ Data/Migrations/20230926200319_v1.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud_aspnet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 80, nullable: false),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubGroups_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    GroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    SubGroupId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Cost = table.Column<double>(type: "REAL", nullable: false),
+                    ProfiMargin = table.Column<int>(type: "INTEGER", nullable: false),
+                    SaleValue = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Products_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Products_SubGroups_SubGroupId",
+                        column: x => x.SubGroupId,
+                        principalTable: "SubGroups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_GroupId",
+                table: "Products",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Products_SubGroupId",
+                table: "Products",
+                column: "SubGroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubGroups_GroupId",
+                table: "SubGroups",
+                column: "GroupId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "SubGroups");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
