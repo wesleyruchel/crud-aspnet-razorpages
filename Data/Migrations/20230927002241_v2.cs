94803bab5ce910fb7f282416fccd41b3437b06ed
wesleyruchel <wesleyruchel@gmail.com>
2023-09-26 21:34:43 -0300

    update migrations

---
+++ Data/Migrations/20230927002241_v2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud_aspnet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class v2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ProfiMargin",
+                table: "Products",
+                type: "REAL",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProfiMargin",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "REAL");
+        }
+    }
+}
---
+++ Data/Migrations/20230927002758_v3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace crud_aspnet.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+               INSERT INTO Groups (Name) VALUES ('Grupo 1');
+               INSERT INTO SubGroups (Name, GroupId) VALUES ('Sub 1', (SELECT last_insert_rowid()));
+
+               INSERT INTO Groups (Name) VALUES ('Grupo 2');
+               INSERT INTO SubGroups (Name, GroupId) VALUES ('Sub 2', (SELECT last_insert_rowid()));
+               INSERT INTO SubGroups (Name, GroupId) VALUES ('Sub 3', (SELECT last_insert_rowid()));
+
+            ");
+        }
+    }
+}
